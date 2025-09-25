f3d698bf38430c4d28497dbe0c47885367daf87c
Christian <39684482+cfalucho@users.noreply.github.com>
2025-09-24 23:09:26 -0700

    Built resizeArray and realloc

--- Week4/homework#2/array.cpp
+++ Week4/homework#2/array.cpp
@@ -4,12 +4,18 @@
 using namespace std;
 
 void addItem(string*&, string);
-void removeItem(string*, string);
-int getNumOfItems();
-bool isHalfFull();
-void resizeArr(string*&);
+void removeItem(string*&, string);
 void listItems(string*);
-bool checkMemAlloc(string*&);
+int getNumOfItems();
+void clearCart(string*&);
+// void saveCart();
+
+void realloc(string*&, int);
+void resizeMemory(string*&);
+bool isMemAllocSucc(string*&);
+double isMemoryUsage();
+int itemFound(string*&, string);
+
 
 int capacity = 5;             // initialize size of 5
 int numOfElements = 0;
@@ -17,53 +23,118 @@ int numOfElements = 0;
 int main(){
     
     string* ptr = new string[capacity];
+
+    
     
     addItem(ptr, "Watermelon");
-    addItem(ptr, "Watermelon");
-    addItem(ptr, "Watermelon");
-    addItem(ptr, "Watermelon");
-    addItem(ptr, "Watermelon");
-
-   
-  
-
-
     listItems(ptr);
+
+    addItem(ptr, "Green Apple");
+    listItems(ptr);
+
+    addItem(ptr, "Oranges");
+    listItems(ptr);
+
+    addItem(ptr, "Cherry");
+    listItems(ptr);
+
+    addItem(ptr, "Strawberry");
+    listItems(ptr);
+
+    addItem(ptr, "Bananas");
+    listItems(ptr);
+
+    addItem(ptr, "Raspberry");
+    listItems(ptr);
+
+
+    // cout << itemFound(ptr, "Oranges") << "\n";
+    // removeItem(ptr, "cherry");
+    removeItem(ptr, "Cherry");
+    removeItem(ptr, "Watermelon");
+    removeItem(ptr, "Raspberry");
+    removeItem(ptr, "Green Apple");
+    removeItem(ptr, "Oranges");
+    removeItem(ptr, "Strawberry");
+    removeItem(ptr, "Bananas");
+    removeItem(ptr, "Bananas");
+    removeItem(ptr, "cherry");
+  
+    listItems(ptr);
+
 } 
 
 
+
 void addItem(string*& ptr, string item){
-    // Check if capacity is full
-    // if (numOfElements == capacity){
-    //     cout << "Cannont add item. Capacity is full.";
-    //     exit(EXIT_SUCCESS);
-    // }
-    
-    if(isHalfFull()){
-        resizeArr(ptr);
+    // Check the usage of memory
+    double memory = isMemoryUsage();
+   
+    if(memory == 100){
+        cout << "Cannot add item. Capacity is full.\n";
+        EXIT_FAILURE;
     }
-    ptr[numOfElements++] = item;
+    else if (memory >= 90 && memory < 100){
+        cout << "Low memory space.\n";
+        ptr[numOfElements++] = item;
+        return;
+    }
+    else if (memory >= 50 && memory < 90 ){
+        cout << "Resizing memory to increase capacity...\n";
+        resizeMemory(ptr);
+        ptr[numOfElements++] = item;
+        return;
+    }
+    else{
+        ptr[numOfElements++] = item;
+        return;
+    }
 }
 
-int getNumOfItems(int numOfElements){
+int getNumOfItems(){
     return numOfElements;
 }
 
-bool isHalfFull(){
-     bool isHalfFull       = false;
-    
-    if (numOfElements == capacity /2 ){
-        isHalfFull = true;
+void removeItem(string*& ptr, string item){
+    int itemLocation = itemFound(ptr, item);
+    int lastElement = numOfElements - 1;
+
+    // Check if cart is empty
+    if (numOfElements == 0)
+    {
+        cout << "Cart is empty.\n";
+        return;
     }
-    return isHalfFull;
+    
+    if (itemLocation >= 0 && itemLocation <= lastElement)
+    {
+        realloc(ptr, itemLocation);
+        numOfElements--;
+    }
+    else{
+        cout << item << " not found.\n";
+    }
 }
 
-void resizeArr(string*& ptr){
-    int newCapacity = capacity * 2;
-    // Create new array on the heap
-    string* newArr = new string[newCapacity];
+int itemFound(string*& ptr, string item){
+    int itemLocation = 0;
 
-    if (checkMemAlloc(newArr)){
+    for (int index = 0; index < numOfElements; index++)
+    {
+        if (item == ptr[index]){
+            itemLocation = index;
+            cout << item << " found in index " << itemLocation << "\n";
+            return itemLocation;
+        }
+    }
+    return -1;
+}
+
+void resizeMemory(string*& ptr){
+    int newCapacity = capacity * 2;                 // Capacity doubles
+    string* newArr = new string[newCapacity];       // Create new pointer array on the heap
+
+    if (isMemAllocSucc(newArr)){
         // copy old array elements to new array
         for (int i = 0; i < numOfElements; i++){
             newArr[i] = ptr[i];
@@ -71,25 +142,24 @@ void resizeArr(string*& ptr){
         delete[] ptr;
         ptr = newArr;
         capacity = newCapacity;
-
     }
     else{
-        cout << "No new array created.\n";
-   
+        cout << "No new memory created.\n";   
     }
 }
 
 void listItems(string* ptr){
-    for (int i = 0; i < capacity; i++)
-    {
+    for (int i = 0; i < capacity; i++){
         cout << i << ": " << ptr[i] << "\n";
     }
     cout << "\n";
     cout << "# of Elems: " << numOfElements 
-         << "\t\t" << "Capacity: " << capacity << "\n";
+         << "\t" << "Capacity: " << capacity 
+         << "\t" << "Memory usage: " << isMemoryUsage() << "%"
+         << "\n\n";
 }   
 
-bool checkMemAlloc(string*& ptr){
+bool isMemAllocSucc(string*& ptr){
     if(ptr){
         cout << "Memory allocation on heap successful.\n";
         return true;
@@ -97,4 +167,37 @@ bool checkMemAlloc(string*& ptr){
         cout << "Failed memory allocation.\n";
         return false;
     }
+}
+
+double isMemoryUsage(){
+    double isMemoryUsage = (static_cast<double>(numOfElements) / capacity) * 100;
+    return isMemoryUsage;
+}
+
+void clearCart(string*& ptr){
+    cout << "Emptying the items in the cart.\n";
+    numOfElements = 0;
+    delete[] ptr;
+}
+
+
+void realloc(string*& ptr, int itemToDelete){
+    int indexBeforeItem = 0;                            // First element of the array
+    int indexAfterItem = itemToDelete + 1;              // Element after the item to delete
+    string* newArr = new string[capacity];              // Create new pointer array on heap
+
+    cout << "Moving all elements to a new array pointer except"
+         << " the item to delete.\n";
+
+    // Copy first element to itemToDelete - 1
+    for (; indexBeforeItem < itemToDelete; indexBeforeItem++){
+        newArr[indexBeforeItem] = ptr[indexBeforeItem];
+    }
+
+    // Copy element after the itemToDelete to last element
+    for (; indexAfterItem < numOfElements; indexAfterItem++){
+        newArr[indexAfterItem - 1] = ptr[indexAfterItem];
+    }
+    delete [] ptr;
+    ptr = newArr;
 }
